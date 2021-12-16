920880606a2890752f432307a7ed80bccff24f79
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-12-16 01:57:27 +0100

    Removed Footer & Nav

--- src/Components/BasePageLayout/BasePageLayout.tsx
+++ src/Components/BasePageLayout/BasePageLayout.tsx
@@ -3,46 +3,19 @@ import styles from './BasePageLayout.module.scss';
 
 interface IBasePageLayout {
 	children: any;
-	showNavigation?: boolean;
-	showFooter?: boolean;
 }
 const BasePageLayout: React.FunctionComponent<IBasePageLayout> = ({
 	children,
-	showNavigation,
-	showFooter,
 }) => {
 	return (
 		<section className={styles.basePageLayout}>
-			{showNavigation && (
-				<>
-					<div className={styles.tabletAndAboveOnly}>
-						{/* <DesktopNavigation /> */}
-						desktop nav
-					</div>
-					<div className={styles.mobileOnly}>
-						{/* <MobileNavigation /> */}
-						mobile nav
-					</div>
-				</>
-			)}
+			<h1>Chocolate City Studios</h1>
 			<main className={styles.children}>{children}</main>
-			{showFooter && (
-				<>
-					<div className={styles.tabletAndAboveOnly}>
-						{/* <DesktopFooter /> */}Desktopfooter
-					</div>
-					<div className={styles.mobileOnly}>
-						{/* <MobileFooter /> */}mobilefooter
-					</div>
-				</>
-			)}
 		</section>
 	);
 };
 
 BasePageLayout.defaultProps = {
-	showFooter: true,
-	showNavigation: true,
 	children: <Fragment />,
 };
 
